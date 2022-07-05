f1cb60b5f4777b4c35f54da40a765f03a2e07602
Denis Doroshenko <ddtlt1984@gmail.com>
2022-07-05 23:59:51 +0400

    second task completed

--- src/test/java/ReadFileTests.java
+++ src/test/java/ReadFileTests.java
@@ -1,9 +1,25 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ReadFileTests {
 
     @Test
-    public void secondLineShouldHabAlloha(){
-      //write here second task
+    public void secondLineShouldHabAlloha() throws FileNotFoundException {
+        File file = new File("src/test/resources/ReadFileTests.txt");
+        Scanner scanner = new Scanner(file);
+        List<String> strings = new ArrayList<>();
+
+        while (scanner.hasNextLine()) {
+            strings.add(scanner.nextLine());
+        }
+
+        assertTrue(strings.get(1).contains("аллоха"));
     }
+
 }
