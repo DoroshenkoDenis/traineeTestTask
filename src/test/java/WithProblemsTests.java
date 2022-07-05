bceacad45eb70c50221dc57474573385850cc8e5
Denis Doroshenko <ddtlt1984@gmail.com>
2022-07-05 22:59:34 +0400

    first task completed

--- src/test/java/WithProblemsTests.java
+++ src/test/java/WithProblemsTests.java
@@ -1,40 +1,37 @@
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WithProblemsTests {
 
-    private final String CONST = "const";
+    private final String CONST = "newValue";
     private final static String bd = "pui";
 
     @Test
-    public void equalsOneToOne(){
+    public void equalsOneToOneString() {
         assertEquals("1", "1");
     }
 
 
     @Test
-    public void assignValueToConstVar(){
-        CONST = "newValue";
+    public void assignValueToConstVar() {
         assertEquals("newValue", CONST);
     }
 
     @Test
-    public static void equalsOneToOne(){
+    public void equalsOneToOneDigits() {
         assertEquals(1, 1);
     }
 
 
-
     @Test
-    public void stringsMustBeEquals(){
+    public void stringsMustBeEquals() {
         String res = "a";
-
-        if (bd == new String("pui")) {
+        if (bd.equals("pui")) {
             res = "asd";
         }
 
@@ -42,11 +39,10 @@ public class WithProblemsTests {
     }
 
     @Test
-    public void successfullyRemovingFirstElementFromList(){
-        List<String> sourceData = List.of("1", "viskas", "chupocabra");
-        for (String element: sourceData){
-            sourceData.remove(element);
-        }
+    public void successfullyRemovingFirstElementFromList() {
+        List<String> sourceData = new ArrayList<>(List.of("1", "viskas", "chupocabra"));
+        sourceData.remove(0);
+
         assertFalse(sourceData.contains("1"));
     }
 
